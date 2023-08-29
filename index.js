662f5a1bfa273022cc518fc9afff6754733b9fd6
IvanGerman <ivgvi@mail.ru>
2023-08-29 19:31:03 +0700

    feat: using the option waitUntil load

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const puppeteer = require('puppeteer');
   
   const browser = await puppeteer.launch( { headless: false } );
   const page = await browser.newPage();
-  await page.goto('https://www.ozon.ru/brand/soul-way-100258413/');
+  await page.goto('https://www.ebay.de/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&_nkw=Schleifpapier+&_sacat=0', {
+    waitUntil: 'load'
+  });
   //await page.screenshot({ path: 'mywebsite.png'});
   
   const grabParagraph = await page.evaluate( () => {
-    const pgTag = document.querySelectorAll('span');
+    const pgTag = document.querySelectorAll('.s-item__seller-info-text');
     let spanInnTextArr = [];
-    pgTag.forEach((span, index) => {
-      if (span.innerText.includes('отзыв')) {
-        spanInnTextArr.push(span.innerText + String(index))
-      }
-    })
+    pgTag.forEach((p) => {
+      spanInnTextArr.push(p.innerText)
+     })
     return spanInnTextArr;
+;
   });
 
   console.log(grabParagraph);
-  await browser.close();
+  //await browser.close();
 
 })();
