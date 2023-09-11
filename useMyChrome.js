7418a9ec48d80abaad8858550364f4890607d029
IvanGerman <ivgvi@mail.ru>
2023-09-11 19:12:09 +0700

    feat: getting title, link, price and bonus value from the items

--- useMyChrome.js
+++ useMyChrome.js
@@ -16,22 +16,46 @@ puppeteer.launch( {
    } ).then(async browser => {
   console.log('Running tests..')
   const page = await browser.newPage()
-  await page.goto('https://ozon.ru/')
-  //await page.goto('https://www.ozon.ru/brand/soul-way-100258413/')
+  //await page.goto('https://ozon.ru/')
+  await page.goto('https://www.ozon.ru/brand/soul-way-100258413/')
   await page.waitForTimeout(5000)
   await page.screenshot({ path: 'testresult2.png', fullPage: true })
 
   const grabParagraph = await page.evaluate( () => {
     alert('here---');
 
-    const pgTag = document.querySelectorAll('.dl1.d3l.l3d.dx0 span');
+    const pgTag = document.querySelectorAll('.e5o .dl1.d3l.l3d.dx0 span');
 
-    let hrefArr = []
-    Array.from(pgTag).forEach(element => { 
-      hrefArr.push(element.innerText);
+    let hrefArr = [];
+    let linksArr = [];
+    let singleProductData = {};
+    Array.from(pgTag).forEach(bonusSpan => { 
+      if ( bonusSpan.innerText.includes('отзыв') ) {
+        hrefArr.push(bonusSpan.innerText);
+        let linkParentOfItem = bonusSpan.closest('a');
+        if (linkParentOfItem)  {
+
+          let productTitle = linkParentOfItem.nextElementSibling.children[2].firstChild.firstChild.innerText;
+          let productPrice = linkParentOfItem.nextElementSibling.firstChild.firstChild.firstChild.innerText;
+          let bonusValue = bonusSpan.innerText;
+          let linkToProduct = `ozon.ru${linkParentOfItem.getAttribute("href")}`;
+
+          singleProductData.productTitle = productTitle;
+          singleProductData.linkToProduct = linkToProduct;
+          
+          productPrice = productPrice.replace(/\s+/g, '');
+          let numberedProductPrice = productPrice.substring(0, productPrice.length-1);
+          singleProductData.productPrice = Number(numberedProductPrice);
+
+          singleProductData.bonusValue = Number(bonusValue.substring(0, bonusValue.indexOf(' ')));
+
+          linksArr.push( {...singleProductData} );
+        }
+      }
+      
     });
-    // let linksArr = [];
-    // pgTag.forEach((bonusSpan) => {
+
+    // Array.from(pgTag).forEach((bonusSpan) => {
     //   console.log('bonusSpan---', bonusSpan);
     //   if ( bonusSpan.innerText.includes('отзыв') ) {
     //     let linkParentOfItem = bonusSpan.closest('.im2');
@@ -44,7 +68,7 @@ puppeteer.launch( {
     //   }   
     //  });
      
-    return  hrefArr;
+    return  linksArr;
 ;
   });
   
@@ -55,10 +79,10 @@ puppeteer.launch( {
   //   offers.push({'linkToProduct': element});
   // });
 
-  // fs.writeFile( 'bonusoffers.json', JSON.stringify(offers, null, 2), (err) => {
-  // if (err) { throw err };
-  //  console.log('file saved');
-  // })
+  fs.writeFile( 'bonusoffers.json', JSON.stringify(grabParagraph, null, 2), (err) => {
+  if (err) { throw err };
+   console.log('file saved');
+  })
   //fs.writeFile( 'bonusangebots.txt',  grabParagraph.toString() ,() => {});
   //await browser.close()
   console.log(`All done`)
